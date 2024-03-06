cc5de2c6b32e31e10f9648241f97f03e0b693e23
Alan Reji <143480799+alanreji2004@users.noreply.github.com>
2024-03-06 21:39:36 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+def get_weather(city):
+    api_key = "ba3cc52f24f99f5c0045631fbfcf3311"
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+    
+    response = requests.get(url)
+    data = response.json()
+    
+    if data["cod"] == 200:
+        weather_info = {
+            "city": city,
+            "description": data["weather"][0]["description"],
+            "temperature": data["main"]["temp"],
+            "humidity": data["main"]["humidity"],
+            "wind_speed": data["wind"]["speed"],
+        }
+        return weather_info
+    else:
+        return None
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        city = request.form['city']
+        weather = get_weather(city)
+        if weather:
+            return render_template('index.html', weather=weather)
+        else:
+            return render_template('index.html', error="City not found or error in fetching weather information.")
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
